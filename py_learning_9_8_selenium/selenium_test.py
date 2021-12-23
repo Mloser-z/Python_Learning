2506c3424d26d0ce6876c6b6f48c7c99489d67d0
Zhang Hao <zh975525512@163.com>
2021-12-23 11:40:13 +0800

    selenium库一些改变

--- py_learning_9_8_selenium/selenium_test.py
+++ py_learning_9_8_selenium/selenium_test.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
 if __name__ == "__main__":
-    driver = webdriver.Chrome(executable_path="D:/chromedriver/chromedriver.exe")
+    driver = webdriver.Chrome(executable_path="D:/chromedriver/chromedriver_version_96/chromedriver.exe")
     driver.get("https://baidu.com")
     assert u'百度' in driver.title
-    elem = driver.find_element_by_name("wd")
+    elem = driver.find_element(By.ID, "wd")
     elem.clear()
     elem.send_keys(u"网络爬虫")
     elem.send_keys(Keys.RETURN)
--- py_learning_9_8_selenium/whu_login.py
+++ py_learning_9_8_selenium/whu_login.py
@@ -1,15 +1,16 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.by import By
 
 if __name__ == "__main__":
-    driver = webdriver.Chrome(executable_path="D:/chromedriver/chromedriver.exe")
+    driver = webdriver.Chrome(executable_path="D:/chromedriver/chromedriver_version_96/chromedriver.exe")
     url = "https://cas.whu.edu.cn/authserver/login?service=https%3A%2F%2Fehall.whu.edu.cn%3A443%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.whu.edu.cn%2Fnew%2Findex.html"
-    user_name = "2020302111298"
-    password = "21341X"
+    user_name = input("user")
+    password = input("password")
     driver.get(url)
-    user_name_input = driver.find_element_by_id("username")
-    password_input = driver.find_element_by_id("password")
-    login_button = driver.find_element_by_tag_name("button")
+    user_name_input = driver.find_element(By.ID, "username")
+    password_input = driver.find_element(By.ID, "password")
+    login_button = driver.find_element(By.TAG_NAME, "button")
     user_name_input.send_keys(user_name)
     time.sleep(2)
     password_input.send_keys(password)
